2b2985afd46dc072c010cab6d4f723505bf54fa7
MosDevx <mosesk.wamae@gmail.com>
2023-02-17 23:53:02 +0300

    fix variable name in main.rb

--- main.rb
+++ main.rb
@@ -6,5 +6,5 @@ person = Person.new(age: 22, name: 'maximilianus')
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 capitalized_trimmed_person.correct_name
