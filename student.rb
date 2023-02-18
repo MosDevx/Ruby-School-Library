805d691f43c6f7776121adca36c185df094c0764
MosDevx <mosesk.wamae@gmail.com>
2023-02-18 10:54:36 +0300

    create assc has-many/belongs-to btwn Classroom & Student

--- student.rb
+++ student.rb
@@ -6,6 +6,7 @@ class Student < Person
   def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.inlcudes?(self)
   end
 
   def play_hooky
