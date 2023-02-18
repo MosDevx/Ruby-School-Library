8abea6b963a9dbacdde5441ca736d1579515c9b0
MosDevx <mosesk.wamae@gmail.com>
2023-02-18 13:55:58 +0300

    fix student rbb

--- student.rb
+++ student.rb
@@ -1,19 +1,18 @@
-require './Person'
+require './person'
+require './classroom.rb'
 
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom
 
-  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
+  def initialize(age:, classroom:, name:'Unknown', parent_permission:true)
     super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯(ツ)/¯'
   end
-
-  def classroom=(classroom)
-    @classroom = classromm
-    classroom.students.push(self) unless classroom.students.inlcudes?(self)
-  end
 end
+
+
